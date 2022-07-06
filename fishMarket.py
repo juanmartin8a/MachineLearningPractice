d9befcef2d648b4f88d1502c2cd61a4a71a3f960
Juan Martin Zabala Ochoa <juanma@Juans-MacBook-Pro.local>
2022-07-05 20:00:59 -0500

    fishMarket read data

--- fishMarket.py
+++ fishMarket.py
@@ -1,1 +1,18 @@
-print("hello world")
+import csv
+import pandas as pd
+import sklearn
+
+
+# READ DATASET
+train_x = []
+train_y = []
+test_x = []
+train_y = []
+
+dataset = pd.read_csv('datasets/Fish.csv')
+
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print(x)
+print(y)
