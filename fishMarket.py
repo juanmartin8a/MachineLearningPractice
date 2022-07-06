634ce00c7bfea42bb03779a076a4a85e828324fb
Juan Martin Zabala Ochoa <juanma@Juans-MacBook-Pro.local>
2022-07-06 10:31:54 -0500

    fishMarket encode data

--- fishMarket.py
+++ fishMarket.py
@@ -1,6 +1,5 @@
-import csv
 import pandas as pd
-import sklearn
+import numpy as np
 
 
 # READ DATASET
@@ -14,5 +13,29 @@ dataset = pd.read_csv('datasets/Fish.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-print(x)
-print(y)
+
+# ENCODE DATA
+# one hot encode categorical data (fish specie)
+fish_species = []
+
+for i in x:
+  if (i[0] not in fish_species):
+    fish_species.append(i[0])
+
+# one hot encode algorithm
+oneHotEncode = {}
+
+for i, fish_specie in enumerate(fish_species):
+  if (fish_specie not in oneHotEncode.keys()):
+    oneHotEncode[fish_specie] = [0]*len(fish_species)
+    oneHotEncode[fish_specie][i] = 1
+
+# replace list item to encode with one hot encoded array
+new_x = np.empty((0, len(fish_species) + len(x[0][1:])), dtype=int)
+
+for i, array in enumerate(x):
+  if (array[0] in oneHotEncode.keys()):
+    new_array = np.append(oneHotEncode[array[0]], array[1:])
+    new_x = np.append(new_x, np.array([new_array]), axis=0)
+
+x = new_x
